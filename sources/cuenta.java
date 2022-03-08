13c414071b9a13c44c8b290bb410298ef96ab1e0
SalahSenhaji <72514868+SalahSenhaji@users.noreply.github.com>
2022-03-08 21:57:20 +0300

    Add files via upload

---
+++ sources/cuenta.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+    public class Cuenta {
+    
+    private String Nombre_usuario;
+    private String Contraseña;
+    }
+
+    public Cuenta(String Nombre_usuario, String Contraseña) {
+    
+        this.Nombre_usuario = Nombre_usuario;
+        this.Contraseña = Contraseña;
+    }
+
+public void loguearse(String Nombre_usuario, String Contraseña){
+
+}
