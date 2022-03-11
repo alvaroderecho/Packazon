43b6ed31ab6dd6d2537543a3bccadce014fd7c0b
SalahSenhaji <72514868+SalahSenhaji@users.noreply.github.com>
2022-03-11 20:41:50 +0300

    Add files via upload

--- Usuarios/Cuenta.java
+++ Usuarios/Cuenta.java
@@ -1,20 +1,41 @@
 package Usuarios;
 
-
-import java.util.List;
-
     public class Cuenta {
     
     private String Nombre_usuario;
-    private String Contraseña;
-    }
-
-    public Cuenta(String Nombre_usuario, String Contraseña) {
+    private String Contrasenia;
+    
+    //constructor de la clase cuenta
+    public Cuenta(String Nombre_usuario, String Contrasenia) {
     
         this.Nombre_usuario = Nombre_usuario;
-        this.Contraseña = Contraseña;
+        this.Contrasenia = Contrasenia;
     }
 
-public void loguearse(String Nombre_usuario, String Contraseña){
+    //funcion para añadir un nombre de usuario
+    public void SetNombreUsuario(String Nombre_usuario) {
+        if(Nombre_usuario==null) return;
+        this.Nombre_usuario = Nombre_usuario;
+    }
+
+     //funcion para añadir una contrasenia
+     public void SetContrasenia(String Contrasenia) {
+        if(Contrasenia==null) return;
+        this.Contrasenia = Contrasenia;
+
+    }
+
+
+
+//funcion para acceder al sistema 
+public boolean loguearse(String Nombre_usuario, String Contrasenia){
+    if(this.Nombre_usuario !=Nombre_usuario ||  this.Contrasenia != Contrasenia) return false;
+
+    return true;
+}
+
+
+
+
 
 }
