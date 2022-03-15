2591db76af0a638439fe3fddff2cce49e6afa001
SalahSenhaji <72514868+SalahSenhaji@users.noreply.github.com>
2022-03-15 21:52:14 +0300

    Add files via upload

---
+++ Practica/Usuarios/Repartidor.java
@@ -0,0 +1,23 @@
+package Usuarios;
+import Productos.*;
+
+public class Repartidor {
+    String NumTelefono;
+    Boolean Alta;
+
+
+//constructor de la clase repartidor
+public Repartidor(String NumTelefono, Boolean Alta) {
+
+    this.NumTelefono = NumTelefono;
+    this.Alta = Alta;
+    }
+
+
+    /*
+public Boolean MarcarPaquete(Paquete p) {
+
+    return true;
+}
+*/
+}
