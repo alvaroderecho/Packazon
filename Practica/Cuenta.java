eecf0e4055e7ad0dfa75b0e3171864b1bdc615ca
SalahSenhaji <72514868+SalahSenhaji@users.noreply.github.com>
2022-04-01 19:06:05 +0200

    Add files via upload

---
+++ Practica/Cuenta.java
@@ -0,0 +1,49 @@
+package Usuarios;
+
+import java.text.Normalizer;
+
+public class Cuenta {
+    
+    protected String nombre_usuario;
+    protected String contrasenia;
+    
+    //constructor de la clase cuenta
+    public Cuenta(String Nombre_usuario, String Password) {
+    
+        this.nombre_usuario = Nombre_usuario;
+        this.contrasenia = Password;
+    }
+
+    //funcion para añadir un nombre de usuario
+    public void SetNombreUsuario(String Nombre) {
+        if(Nombre==null) return;
+        this.nombre_usuario = Nombre;
+    }
+
+     //funcion para añadir una contrasenia
+     public void SetContrasenia(String Password) {
+        if(Password==null) return;
+        this.contrasenia = Password;
+
+    }
+
+    //funcion que devuelve el nombre de un usuario
+    public String GetNombreUsuario() {
+        return this.nombre_usuario;
+    }
+
+    //funcion que devuelve la contraseña de un usuario
+    public String GetContrasenia() {
+        return this.contrasenia;
+    }
+
+
+
+//funcion para acceder al sistema 
+public boolean loguearse(String Nombre, String Password){
+    if(this.nombre_usuario !=Nombre ||  this.contrasenia != Password) return false;
+
+    return true;
+}
+
+}
