1b0e4b960475d262f0f0bb7417cc32e5daf356dc
Alvaro Derecho <alvaroderechoramos@gmail.com>
2022-04-08 18:37:04 +0200

    interfaces inicio

---
+++ src/Interfaces/Login.java
@@ -0,0 +1,16 @@
+package Interfaces;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Login {
+    public static void main(String[] a) {
+        LoginFrame frame = new LoginFrame();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 370, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Pantalla completa
+ 
+    } 
+}
