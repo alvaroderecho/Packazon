e81ea30acfa0d31bce3eb184386783345b6b060b
SalahSenhaji <72514868+SalahSenhaji@users.noreply.github.com>
2022-04-29 17:10:50 +0200

    Add files via upload

---
+++ Status.java
@@ -0,0 +1,17 @@
+package Sistema;
+
+/**
+ * Enumeracion de los tipos de estados de las funciones
+ * @author Oscar Riquelme, Pablo Sanz, Javier Simancas
+ */
+public enum Status {
+	/**
+	 * El metodo funciona correctamente
+	 */
+	OK,
+	
+	/**
+	 * El metodo no funciona correctamente
+	 */
+	ERROR
+}
